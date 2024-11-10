70b420fc8ca32461a56f6ad4f35938ec65357647
Bryan Wu <bryanwu123456@gmail.com>
2024-11-10 10:58:13 -0800

    rename backend to backend

--- backend/views_autogen.py
+++ backend/views_autogen.py
@@ -1,5 +1,5 @@
 """
-URL configuration for land_degradation project.
+URL configuration for backend project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
