04aff4ee2fea2e4644945c89bfa48fa372a15f35
Bryan Wu <bryanwu123456@gmail.com>
2024-11-10 14:34:32 -0800

    add endpoint

--- backend/urls.py
+++ backend/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('api/search-datasets/', views.search_datasets, name='search_datasets'),
+    path('api/viz-data/', views.vizualize_dataset, name='vizualize_dataset'),
 ]
 
 # settings.py additions
